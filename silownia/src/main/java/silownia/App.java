fdf4f6d4f2d2cf3ed19f7ab66ad44cbe087cca96
damian11 <damian.kasztelan1@op.pl>
2016-01-30 00:41:10 +0100

    utworzenie projektu

---
+++ silownia/src/main/java/silownia/App.java
@@ -0,0 +1,13 @@
+package silownia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
