7d4eac2e146418e9f0b06f7d03301cccd46ac5ed
damian11 <damian.kasztelan1@op.pl>
2016-01-31 18:42:01 +0100

    wypełnienie klasy Klient.java

--- silownia/src/main/java/com/silownia/hibernate/domain/Klient.java
+++ silownia/src/main/java/com/silownia/hibernate/domain/Klient.java
@@ -7,5 +7,42 @@ import javax.persistence.*;
 	@NamedQuery(name = "klient.pobierzKlientPoImie", query = "Select k from Klient k where k.imie= :imie")
 })
 
+public class Klient {
+    private Long idKlient;
+    private String imie;
+    private String nazwisko;
+    
+	public Klient(){}
+	
+	public Klient(String imie, String nazwisko){
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+	}
+	
+	
+	
+	
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getidKlient() {
+        return idKlient;
+    }
+    public void setId_klient(Long id_klient) {
+        this.idKlient = idKlient;
+    }
 
+    public String getImie() {
+        return imie;
+    }
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
 }
