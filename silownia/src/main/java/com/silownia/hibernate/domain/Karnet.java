aafbf05f78203b2bbdb5a88d32f54559771d1814
damian11 <damian.kasztelan1@op.pl>
2016-01-31 19:18:24 +0100

    wypełnienie klasy Karnet.java

--- silownia/src/main/java/com/silownia/hibernate/domain/Karnet.java
+++ silownia/src/main/java/com/silownia/hibernate/domain/Karnet.java
@@ -24,5 +24,41 @@ public class Karnet {
 		this.cena = cena;
 	}
     
-   
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId_karnet(){
+        return id_karnet;
+    }
+    public void setId_karnet(Long id_karnet){
+        this.id_karnet = id_karnet;
+    }
+
+    public String getOpis(){
+        return opis;
+    }
+    public void setOpis(String opis){
+        this.opis = opis;
+    }
+
+    public String getRodzaj(){
+        return rodzaj;
+    }
+    public void setRodzaj(String rodzaj){
+        this.rodzaj = rodzaj;
+    }
+
+    public double getCena(){
+        return cena;
+    }
+    public void setCena(double cena){
+        this.cena = cena;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    public List<Klient> getKlients(){
+        return klienci;
+    }
+    public void setKlients(List<Klient> klienci){
+        this.klienci = klienci;
+    }
 }
